55d9901dd1152d45d655e531aacf066a9bd96b6a
Leandro Mangubat <leandromangubat@hotmail.com>
2023-07-09 15:17:56 -0400

    Updated model files

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-API",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
